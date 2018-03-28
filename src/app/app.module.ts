48248ce629c0159f73b17848d4487158160c3110
Test Man <ttestman4@gmail.com>
2018-03-28 19:48:27 +0530

    package consumed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 // import { Feature1Module } from '../feature1/feature1.module';
 import { NonFunctionalModule } from '../non-functional/non-functional.module';
+import { Lib2Module } from '@ttestman4/lib2';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import { NonFunctionalModule } from '../non-functional/non-functional.module';
     AppRoutingModule,
     NonFunctionalModule,
     // Feature1Module,
+    Lib2Module
   ],
   providers: [],
   bootstrap: [AppComponent]
